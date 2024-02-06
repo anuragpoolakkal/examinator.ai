082afc3b9b9931aa98c0fc775c8fd72ba07c080e
sai-na <saipkmm100@gmail.com>
2024-02-06 23:31:05 +0530

    page pdf code

--- client/app/(root)/test/sai/page.tsx
+++ client/app/(root)/test/sai/page.tsx
@@ -1,7 +1,18 @@
-import React from "react"
+"use client"
+import { useRouter } from "next/navigation"
 
-function page() {
-  return <div>page</div>
+export default function Home() {
+  const router = useRouter()
+
+  return (
+    <main className="flex items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tr from-[#6c63ff] to-[#20bee9]">
+      Examinator.AI
+      <button
+        className="btn btn-primary btn-lg transform transition-transform hover:scale-105 focus:outline-none focus:ring focus:border-primary-dark"
+        onClick={() => router.push("/dashboard")}
+      >
+        Get Started
+      </button>
+    </main>
+  )
 }
-
-export default page
